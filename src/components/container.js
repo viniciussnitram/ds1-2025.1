7f2b13a4aee4097ccbdc04890195c09f5d6dc3a7
Patrick Alves Knidel <patrick.alves.knidel@gmail.com>
2024-12-05 23:34:36 -0300

    adicionando favicon

--- src/components/container.js
+++ src/components/container.js
@@ -1,5 +1,5 @@
 export default function Container({ children }) {
   return (
-    <div className="max-w-[1800px] mx-auto bg-white min-h-screen flex flex-col border-l border-r">{children}</div>
+    <div className="w-full mx-auto bg-white min-h-screen flex flex-col border-l border-r">{children}</div>
   )
 }
